ea03ea24c72aaf904fca56a06b03a873d4b5984c
BudzikM <budzonmichal@gmail.com>
2026-01-29 17:15:20 +0100

    Add test

--- src/cmd/easee_internal_test.go
+++ src/cmd/easee_internal_test.go
@@ -1327,6 +1327,29 @@ func TestEaseeAdapter(t *testing.T) { //nolint:paralleltest
 					},
 				},
 			},
+			{
+				Name: "Token refresh task should call Ping() at configured interval",
+				Setup: serviceSetup(testContainer, "configured", mqttAddr, func(client *mocks.APIClient) {
+					client.On("ChargerConfig", "XX12345").Return(&model.ChargerConfig{}, nil)
+					client.On("ChargerSiteInfo", "XX12345").Return(&model.ChargerSiteInfo{}, nil)
+					client.On("Ping").Return(nil).Maybe()
+				}, signalRSetup(test.DefaultSignalRAddr, nil)),
+				TearDown: []suite.Callback{tearDown("configured"), testContainer.TearDown()},
+				Nodes: []*suite.Node{
+					{
+						InitCallbacks: []suite.Callback{
+							waitForRunning(),
+							func(t *testing.T) {
+								time.Sleep(400 * time.Millisecond)
+							},
+							func(t *testing.T) {
+								client := services.easeeAPIClient.(*mocks.APIClient)
+								client.AssertNumberOfCalls(t, "Ping", 4)
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 
