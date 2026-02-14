b710bdcc495558a5cd0355f858eaf3e2d2ed0eb4
BudzikM <budzonmichal@gmail.com>
2026-02-14 15:39:18 +0100

    Fix

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -12,35 +12,6 @@ import (
 	"github.com/futurehomeno/edge-easee-adapter/internal/routing"
 )
 
-type BudzikFormatter struct {
-	TimestampFormat string
-	LevelDesc       []string
-}
-
-func (f *BudzikFormatter) Format(entry *log.Entry) ([]byte, error) {
-	timestamp := entry.Time.Format(f.TimestampFormat)
-
-	level := "D"
-
-	if int(entry.Level) >= 0 && int(entry.Level) < len(f.LevelDesc) {
-		level = f.LevelDesc[int(entry.Level)]
-	}
-
-	ret := fmt.Appendf(nil, "%s %s %s", timestamp, level, entry.Message)
-	for k, v := range entry.Data {
-		ret = fmt.Appendf(ret, " %s=%v", k, v)
-	}
-
-	ret = fmt.Appendf(ret, "\n")
-
-	return ret, nil
-}
-
-func NewBudzikFormatter() *BudzikFormatter {
-	lvlDesc := []string{"PANIC", "FATAL", "E", "W", "I", "D", "T", "?"}
-	return &BudzikFormatter{TimestampFormat: "01-02 15:04:05", LevelDesc: lvlDesc}
-}
-
 // Execute is an entry point to the edge application.
 func Execute(version string) error {
 	cfg := getConfigService().Model()
@@ -54,9 +25,6 @@ func Execute(version string) error {
 	if err != nil {
 		return fmt.Errorf("build app err: %w", err)
 	}
-
-	log.Infof("\t--- Start Easee v%s ---", version)
-	defer log.Infof("\t+++ Stop Easee v%s +++", version)
 
 	err = rootApp.Run()
 	if err != nil {
