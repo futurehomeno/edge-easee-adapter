989a1cc906a4a9d34c8446df8e322418f9d8d374
BudzikM <budzonmichal@gmail.com>
2026-02-14 14:03:55 +0100

    Fix

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -47,8 +47,6 @@ func Execute(version string) {
 
 	bootstrap.InitializeLogger(cfg.LogFile, cfg.LogLevel, cfg.LogFormat)
 
-	log.SetFormatter(NewBudzikFormatter()) // remove when implemented in
-
 	log.Infof("\t--- Start Easee v%s ---", version)
 	defer log.Infof("\t+++ Stop Easee v%s +++", version)
 
