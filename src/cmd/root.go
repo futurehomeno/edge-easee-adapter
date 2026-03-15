92acc2e292cf41ad42c38dcde7aa2cd2f20d39d8
BudzikM <budzonmichal@gmail.com>
2026-03-15 17:41:16 +0100

    Review

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -66,8 +66,8 @@ func migrateConfig(cfg *config.Config) {
 		migrated = true
 	}
 
-	if cfg.WorkDir == "/var/log/thingsplex/easee" {
-		cfg.WorkDir = "/var/log/futurehome/easee"
+	if cfg.LogFile == "/var/log/thingsplex/easee" {
+		cfg.LogFile = "/var/log/futurehome/easee"
 		migrated = true
 	}
 
